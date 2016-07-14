57d3949d027c73c10329e30922807d9306220681
Drew Conly <atconly@gmail.com>
2016-07-13 22:21:28 -0600

    add hide function and automatic update

--- profile.js
+++ profile.js
@@ -10,13 +10,21 @@ angular.module('profileApp')
 function profileCtrl($http) {
   var profile       = this;
   profile.btnClick  = false;
+  profile.showBtn   = true;
+  profile.hideBtn   = false;
   profile.name      = 'Samuel L. Jackson';
   profile.bio       = "You don't know me";
   profile.books     = 'Catcher in the Rye';
   profile.libraries = 'Angula Mutha Fucka';
 
   profile.showForm  = function() {
-    console.log('show it bitch');
     profile.btnClick = !profile.btnclick;
+    profile.showBtn  = !profile.showBtn;
+    profile.hideBtn  = !profile.hideBtn;
+  }
+  profile.hideForm  = function() {
+    profile.btnClick = false; // why did I need to explicitly declare false here??
+    profile.showBtn  = !profile.showBtn;
+    profile.hideBtn  = !profile.hideBtn;
   }
 }
